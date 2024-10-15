8f135851db9bf403560658771e9c4b9359699077
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-10-16 05:26:53 +0700

    feat: add Marketplace page

---
+++ src/pages/Marketplace.tsx
@@ -0,0 +1,15 @@
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import PageTitle from '../components/PageTitle';
+
+const MarketplacePage = () => {
+    return (
+        <>
+            <PageTitle title='Marketplace' />
+            <Header />
+            <Banner />
+        </>
+    );
+};
+
+export default MarketplacePage;
