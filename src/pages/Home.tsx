625c92d5de183f8eb1a58346d1df27c301d99fdb
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-10-16 05:27:10 +0700

    feat: added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import PageTitle from '../components/PageTitle';
+
+const HomePage = () => {
+    return (
+        <>
+            <PageTitle title='Home' />
+            <div>Home page here</div>
+        </>
+    );
+};
+
+export default HomePage;
