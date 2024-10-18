ac75ca8ae6e8454f4a3b18f280d8f0d6b273804d
nguyenhoangthienchi <jayesd3v@gmail.com>
2024-10-18 15:30:44 +0700

    feat: added Filter Panel and Result Panel to Marketplace page

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,8 +1,11 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import Banner from '../components/Banner';
 import BottomLines from '../components/BottomLines';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import PageTitle from '../components/PageTitle';
+import FilterPanel from '../components/FilterPanel';
+import ResultPanel from '../components/ResultPanel';
 
 const MarketplacePage = () => {
     return (
@@ -10,6 +13,16 @@ const MarketplacePage = () => {
             <PageTitle title='Marketplace' />
             <Header />
             <Banner />
+            <Container className='py-5'>
+                <Row>
+                    <Col xs={12} md={4} lg={3}>
+                        <FilterPanel />
+                    </Col>
+                    <Col xs={12} md={8} lg={9}>
+                        <ResultPanel />
+                    </Col>
+                </Row>
+            </Container>
             <BottomLines />
             <Footer />
         </>
